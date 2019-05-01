5d4158892a336b7341863e70719c287cd2a0bccc
lrufo <lrufo2484@floridapoly.edu>
2019-05-01 14:29:26 -0400

    Controller raycasts to button and timer stops
    The timer now starts and stops when the user presses the button. The
    button can be pressed by raycasting the controller's location on to the
    button.
    
    There's a bug where the button doesn't change color, but that's not a
    major issue.

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -7,26 +7,60 @@ public class timer : MonoBehaviour {
 
 	public float totalTime;
 	public TextMeshProUGUI clockText;
+	public Button pauseButton;
+	public Image buttonImg;						// Button background image that's altered to change the color.
+	public Material[] colorMaterials;
+	public bool paused;
 	// Use this for initialization
 	void Start () {
 		totalTime = 60f * 60f;		// 5 minutes in seconds
 		totalTime += 15f;
-		clockText = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();
+		paused = true;
+		buttonImg = GameObject.Find("Start and Pause").GetComponent<Image>();			// Grab the start and pause button's image
+		clockText = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();			// Grab the text mesh pro GUI component
 		Debug.Log(clockText.name);
+		StartCoroutine(changeColor());
 	}
-	
+	public IEnumerator changeColor()			// Checks of the timer is paused. The button's color is changed and then the thread is put to sleep until paused changes.
+	{
+		if(paused == true)
+		{
+			buttonImg.color = Color.red;
+			//GameObject.Find("Start and Pause").GetComponent<Text>().text = "Start";
+			yield return new WaitUntil(() => paused == false);
+		}
+		if(paused == false)
+		{
+			buttonImg.color = Color.red;
+			//GameObject.Find("Start and Pause").GetComponent<Text>().text = "Pause";
+			yield return new WaitUntil(() => paused == true);
+		}
+		yield return new WaitForSeconds(1/90);
+	}
 	// Update is called once per frame
 	void Update () {
 		// On each frame, update the clock text with the current
-		totalTime -= Time.deltaTime;
-		if(totalTime < 0)
+		if(paused == false)
+			totalTime -= Time.deltaTime;			// Total time minus the time since the last Time.deltaTime
+		if(totalTime < 0)						// If totalTime is less than 0 do something.
 		{
 			Debug.Log("Game Over");
 		}
-		int seconds = (int)(totalTime % 60);
-		int minute = (int)((totalTime/60) % 60);
-		int hour = (int)((totalTime/3600) % 60);
+		int seconds = (int)(totalTime % 60);				// seconds conversion
+		int minute = (int)((totalTime/60) % 60);			// minutes conversion
+		int hour = (int)((totalTime/3600) % 60);			// hours conversion
 
-		clockText.text = hour.ToString() + ":" + minute.ToString() + ":" + seconds.ToString();
+		clockText.text = hour.ToString() + ":" + minute.ToString() + ":" + seconds.ToString();			// Change clock text.
+	}
+
+	// setter for paused boolean
+	public void setPaused()
+	{
+		Debug.Log(paused);
+		if(paused == true)
+			paused = false;
+		else
+			paused = true;
+			Debug.Log(paused);
 	}
 }

d754c04fb51268fa866d7b393ad5cdf44885e1d8
lrufo <lrufo2484@floridapoly.edu>
2019-05-01 09:34:41 -0400

    Added timer and button
    The timer runs on a script that calculates the delta time passed between
    frames and subtracts it from the total time in seconds. That time is
    then mathematically converted into hours, minutes, and seconds and
    displayed to the user.
    
    A digital font I found online was used for the clock's text. It's
    licensed as free for personal and educational use. I had to generate
    a TextMeshPro font asset to use it. It appears to work fine.
    
    A button has been placed in the scen, but it isn't interactable in VR
    yet.

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class timer : MonoBehaviour {
+
+	public float totalTime;
+	public TextMeshProUGUI clockText;
+	// Use this for initialization
+	void Start () {
+		totalTime = 60f * 60f;		// 5 minutes in seconds
+		totalTime += 15f;
+		clockText = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();
+		Debug.Log(clockText.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// On each frame, update the clock text with the current
+		totalTime -= Time.deltaTime;
+		if(totalTime < 0)
+		{
+			Debug.Log("Game Over");
+		}
+		int seconds = (int)(totalTime % 60);
+		int minute = (int)((totalTime/60) % 60);
+		int hour = (int)((totalTime/3600) % 60);
+
+		clockText.text = hour.ToString() + ":" + minute.ToString() + ":" + seconds.ToString();
+	}
+}
